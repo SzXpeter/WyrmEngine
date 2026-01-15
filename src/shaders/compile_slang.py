6915d34ec5f27a14dbf84dea0be171a2ba03bee9
SzXPeter <szabo.peter@students.jedlik.eu>
2026-01-15 20:40:18 +0100

    rewrote slang compilation in python

---
+++ src/shaders/compile_slang.py
@@ -0,0 +1,21 @@
+import os
+import subprocess
+
+entry_points = [
+    "-entry", "vertMain",
+    "-entry", "fragMain"
+]
+
+arguments = [
+    "slangc", "shader.slang",
+    "-target", "spirv",
+    "-profile", "spirv_1_4",
+    "-emit-spirv-directly",
+    "-fvk-use-entrypoint-name",
+]
+arguments.extend(entry_points)
+arguments.extend(["-o", "shader.spv"])
+
+subprocess.run(arguments, check=True)
+
+os.replace("shader.spv", "../../cmake-build-debug/src/shaders/shader.spv")
